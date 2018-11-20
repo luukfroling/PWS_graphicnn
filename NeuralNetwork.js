0420e59297fb1e30dc336c020a71709cb840b1f4
luukfroling <31106485+luukfroling@users.noreply.github.com>
2018-11-20 20:52:13 +0100

    Add files via upload

---
+++ NeuralNetwork.js
@@ -0,0 +1,84 @@
+class neuralNetwork {
+  constructor(sizes){
+    //Create layers:
+    this.layers = new Array();
+    for(let i = 0; i < sizes.length; i++){
+      this.layers.push(new Matrix(sizes[1]));
+    }
+    //Create weights and biases:
+    this.weights = new Array();
+    this.biases = new Array();
+
+    for(let i = 0; i < this.layers.length - 1; i++){
+      this.weights.push(new Matrix(sizes[i+1],sizes[i]));
+      this.biases.push(new Matrix(sizes[i+1],1));
+    }
+  }
+  /* An activation function for the network.
+  *  Go through all the layers and dot multiply. We also need to apply the sigmoid function over every element.
+  */
+  activate(input){
+    this.layers[0] = Matrix.fromArray(input);
+    for(let i = 0; i < this.weights.length; i++){
+      let w = this.weights[i];
+      let x = this.layers[i];
+      this.layers[i+1] = Matrix.product(w, x);
+      this.layers[i+1].add(this.biases[i]);
+      this.layers[i+1].func(sigmoid);
+    }
+    this.output = this.layers[this.layers.length - 1];
+  }
+
+  run(input){
+    this.activate(input);
+    return Matrix.toArray(this.output);
+  }
+  /* A training fucntion using backpropagation.
+  *  We need to loop through every layer, and we need to do the following:
+  * -Transpose The weight matrix so we can dot product the matrix with the errors to get the errors of the
+  * previous layer.
+  * -Transpose the layer so we can dot multiply the error.
+  * - Calculate the dot product of error * input so we get the change
+  * - Calculate the dot product of the transposed weight * error so we get the next error.
+  * - Add the calculated change to the weight matrix we are currently in.
+  */
+  train(input, des){
+    this.activate(input);
+    let error = Matrix.substract(Matrix.fromArray(des), this.output);
+    for(let i = this.weights.length - 1; i > -1; i--){
+      //Transpose:
+      let inT = Matrix.transpose(this.layers[i]);
+      let wT = Matrix.transpose(this.weights[i]);
+      //Product:
+      let change = Matrix.product(error, inT);
+      this.biases[i].add(error);
+      error = Matrix.product(wT, error);
+      //Update weights:
+      this.weights[i].add(change);
+    }
+  }
+
+  /* Visualise the network. Takes a P% graphics input.
+  *  This function therefor requires the P5.js library.
+  *  Returns the adjusted graphics
+  */
+  visualise(g){
+    //Setup the nodes and loop through them
+    for(let i = 0; i < this.layers.length; i++){
+      let xdiv = width / (this.layers.length + 1);
+
+      for(let j = 0; j < this.layers[i].matrix.length; j++){
+        let div = (height) / (this.layers[i].matrix.length + 1);
+        fill(map(this.layers[i].matrix[j][0], 0, 1, 0, 255));
+        ellipse(xdiv + i * xdiv, div + j * div, 10, 10);
+        fill(0);
+        text(roundDecimal(this.layers[i].matrix[j][0]), 20 + xdiv + i * xdiv, div + j * div);
+      }
+    }
+    return g;
+  }
+}
+
+function sigmoid(x) {
+    return 1/(1+Math.pow(Math.E, -x));
+}
---
+++ example1.0.js
@@ -0,0 +1,46 @@
+let n;
+
+let data = [
+  [[0,1,1,0,0,0,0,1], [1,0,0,0,0,0,0,0]], //a
+  [[0,1,1,1,0,0,1,1], [0,0,1,0,0,0,0,0]], //s
+  [[0,1,1,0,1,0,1,1], [0,1,0,0,0,0,0,0]], //k
+  [[0,1,1,1,1,0,1,0], [0,0,0,1,0,0,0,0]], //k
+  [[0,1,1,0,0,1,1,0], [0,0,0,0,1,0,0,0]], //f
+  [[0,1,1,0,0,1,0,0], [0,0,0,0,0,1,0,0]], //d
+  [[0,1,1,1,0,1,1,1], [0,0,0,0,0,0,1,0]], //w
+  [[0,1,1,1,0,1,1,0], [0,0,0,0,0,0,0,1]],
+]
+
+let points;
+
+function setup(){
+
+  //Create neural network
+  n = new neuralNetwork([8, 5, 5, 8]);
+
+  //Train beforehand on the data so we can do drawing only in draw.
+  for(let j = 0; j < 100; j++){
+    for(let i = 0; i < data.length; i++){
+      n.train(data[i][0], data[i][1]);
+    }
+  }
+  console.log(n);
+
+  //Create canvas
+  createCanvas(400,400);
+  pg = createGraphics(width, height);
+  background(100);
+
+
+
+}
+
+function draw(){
+  background(200);
+  image(n.visualise(pg), 0,0);
+}
+
+//Round off to 2 decimal places
+function roundDecimal(d){
+  return (Math.round(d*1000) /1000);
+}
